5d0d2eade23774659f6d31916ac87d5910d67dc0
Ethan <edjordan20@outlook.com>
2026-04-05 12:38:15 -0500

    Added helper function for setting bits

--- sh1106.c
+++ sh1106.c
@@ -171,6 +171,7 @@ void sh1106_update_config(sh1106_t *display, SemaphoreHandle_t i2c_mutex) {
 }
 
 static inline bool bit_check(uint8_t val, uint8_t pos) { return (val & (1 << pos)) > 0x00; }
+static inline bool bit_set(uint8_t val, uint8_t pos, bool on) { return on ? val | (0x01 << pos) : val & ((0x01 << pos) ^ 0xFF); }
 
 sh1106_t *sh1106_new(void) {
     sh1106_t *ptr = malloc(sizeof(sh1106_t));
