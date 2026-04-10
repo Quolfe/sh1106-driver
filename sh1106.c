874e0072726ddf80e8dd38e39a6553802ff456bc
Ethan <edjordan20@outlook.com>
2026-04-10 15:05:10 -0500

    Fixed page math issue

--- sh1106.c
+++ sh1106.c
@@ -493,7 +493,7 @@ void sh1106_draw_bitmap(sh1106_t *display, bitmap_t bitmap, uint8_t x, uint8_t y
 
     xSemaphoreTake(display->frame_mutex, portMAX_DELAY);
     for (uint8_t i = 0; i < bitmap.data_length; i++) {
-        uint8_t page = y / 8 + i / bitmap.width;
+        uint8_t page = (i / bitmap.width + y) / 8;
         uint8_t col = x + i % bitmap.width;
         if (page >= 8)
             break;
